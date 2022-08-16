d06b76f6f1801d84438a804bbaae2ea25ea735f2
mark.HsieH@m1 <mark.hsieh@cerence.com>
2022-08-17 00:38:46 +0800

    Initial Commit

---
+++ FDLS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FDLS
+//
+//  Created by Mark HsieH on 2022/8/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FDLS/FDLSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FDLSApp.swift
+//  FDLS
+//
+//  Created by Mark HsieH on 2022/8/17.
+//
+
+import SwiftUI
+
+@main
+struct FDLSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

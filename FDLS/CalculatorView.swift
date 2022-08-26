9fd52651fa8d35d2c196ce374a8a29c7f1124692
mark.HsieH@m1 <mark.hsieh@cerence.com>
2022-08-26 23:43:09 +0800

    preview devices & dark mode

--- FDLS/CalculatorView.swift
+++ FDLS/CalculatorView.swift
@@ -10,7 +10,17 @@ import SwiftUI
 struct CalculatorView: View {
   var body: some View {
     
-    VStack {
+    VStack (spacing: 12) {
+      
+      Spacer()
+      
+      Text("3.14")
+        .font(.system(size: 76))
+        .minimumScaleFactor(0.5)
+        .lineLimit(1)
+        .frame(maxWidth: .infinity, alignment: .trailing)
+        .padding(.trailing, 4)
+
       CalculatorButtonsRow(
         items: [.command(.clear), .command(.flip), .command(.percent), .op(.divide)])
 
@@ -25,6 +35,7 @@ struct CalculatorView: View {
 
       CalculatorButtonsRow(
         items: [.digit(0), .dot, .op(.equal)])
+
     }
     
   }
@@ -33,5 +44,8 @@ struct CalculatorView: View {
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     CalculatorView()
+    CalculatorView().preferredColorScheme(.dark)
+    CalculatorView().previewDevice("iPhone SE (2nd generation)")
+    CalculatorView().previewDevice("iPhone 13 Pro Max")
   }
 }

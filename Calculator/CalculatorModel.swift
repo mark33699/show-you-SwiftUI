0392544e01ea7ed5336be30cd4a7574358740be7
mark.HsieH@m1 <mark.hsieh@cerence.com>
2022-08-27 23:16:05 +0800

    add @Published

--- Calculator/CalculatorModel.swift
+++ Calculator/CalculatorModel.swift
@@ -10,8 +10,5 @@ import Foundation
 import Combine
 
 class CalculatorModel: ObservableObject {
-  let objectWillChange = PassthroughSubject<Void, Never>()
-  var brain: CalculatorBrain = .left("0") {
-    willSet { objectWillChange.send() }
-  }
+  @Published var brain: CalculatorBrain = .left("0")
 }

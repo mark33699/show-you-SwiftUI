e66e0ab8dad5ae67805b952b4f49b750c90aabad
mark.HsieH@m1 <mark.hsieh@cerence.com>
2022-09-03 01:08:41 +0800

    BGColor out of safe area

--- FDLS/Views/ButtonView.swift
+++ FDLS/Views/ButtonView.swift
@@ -8,21 +8,24 @@
 import SwiftUI
 
 struct ButtonView: View {
-    var body: some View {
-      
+  var body: some View {
+    VStack {
       Button {
         print("tap")
       } label: {
         Text("This is a Button")
           .padding().padding()
-          .background(Color.yellow)
+//            .background(Color.yellow) // out of safe area
+//            .background(Color.yellow.frame(height: .infinity)) // why???
+            .background(Color.yellow.edgesIgnoringSafeArea([])) //....負負得正
       }
-
+      Spacer()
     }
+  }
 }
 
 struct ButtonView_Previews: PreviewProvider {
-    static var previews: some View {
-        ButtonView()
-    }
+  static var previews: some View {
+    ButtonView()
+  }
 }

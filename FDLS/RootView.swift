3119058b0912e80c702939a6a6dbe0e618e27b2c
mark.HsieH@m1 <mark.hsieh@cerence.com>
2022-09-20 22:48:26 +0800

    reorder

--- FDLS/RootView.swift
+++ FDLS/RootView.swift
@@ -45,16 +45,19 @@ struct Lesson {
 }
 
 let lessons = [
-  Lesson(name: "常用Modifier", view: AnyView(CommonView())),
-  Lesson(name: "排列", view: AnyView(VStackView())),
-  Lesson(name: "顯示文字", view: AnyView(TextView())),
-  Lesson(name: "顯示圖片", view: AnyView(ImageView())),
-  Lesson(name: "按鈕", view: AnyView(ButtonView())),
-  Lesson(name: "挑選器", view: AnyView(PickerView())),
-  Lesson(name: "日期挑選", view: AnyView(DatePickerView())),
-//  Lesson(name: "圖片挑選", view: AnyView(EmptyView())),1
-  Lesson(name: "文字輸入", view: AnyView(TextInputView())),
-  Lesson(name: "其他控件", view: AnyView(OtherControlView())),
-  Lesson(name: "對話框", view: AnyView(AlertView())),
-  Lesson(name: "顏色跟形狀", view: AnyView(EmptyView())),
+  Lesson(name: "常用Modifier", view: AnyView(CommonView())), //3
+  Lesson(name: "排列", view: AnyView(VStackView())), //4
+  Lesson(name: "顯示文字", view: AnyView(TextView())), //5
+  Lesson(name: "顯示圖片", view: AnyView(ImageView())), //6
+  Lesson(name: "按鈕", view: AnyView(ButtonView())), //7
+  Lesson(name: "文字輸入", view: AnyView(TextInputView())), //8
+  Lesson(name: "挑選器", view: AnyView(PickerView())), //9
+  Lesson(name: "日期挑選", view: AnyView(DatePickerView())), //10
+  Lesson(name: "對話框", view: AnyView(AlertView())), //11
+  Lesson(name: "其他控件", view: AnyView(OtherControlView())), //12
+  Lesson(name: "顏色", view: AnyView(ColorView())), //13
+  Lesson(name: "形狀", view: AnyView(ShapeView())), //14
+  Lesson(name: "其他View", view: AnyView(OtherView())), //15
+  
+  Lesson(name: "圖片挑選", view: AnyView(ImagePickerView())),
 ]
---
+++ FDLS/Views/ImagePickerView.swift
@@ -0,0 +1,20 @@
+//
+//  ImagePickerView.swift
+//  FDLS
+//
+//  Created by Mark HsieH on 2022/9/20.
+//
+
+import SwiftUI
+
+struct ImagePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImagePickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagePickerView()
+    }
+}

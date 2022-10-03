722f54bf3650da21110ea106becb34c30698b670
mark.HsieH@m1 <mark.hsieh@cerence.com>
2022-10-03 23:54:20 +0800

    hidden test

--- FDLS/LabView.swift
+++ FDLS/LabView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct LabView: View {
+  
+  @State var opacityValue = 0.004167
+  
     var body: some View {
-      HStack {
-        Spacer()
-        ScrollView {
-          ForEach(0..<100) {
-            Text("\($0)")
+      
+      let isShow = false
+      
+      VStack {
+        Color.pink
+          .overlay(Text("\(opacityValue)"))
+        
+//        if isShow {
+//          Color.pink
+//        }
+        
+        Color.pink
+          .onTapGesture {
+            opacityValue -= 0.000001
           }
-        }.padding(.vertical, 1)
+          .opacity(isShow ? 1 : opacityValue)
+        
+        Color.pink
       }
     }
 }

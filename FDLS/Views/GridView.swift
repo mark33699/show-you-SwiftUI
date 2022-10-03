c3e9a0b0d88243d4621151194bd183fae01c4076
mark.HsieH@m1 <mark.hsieh@cerence.com>
2022-10-04 02:25:03 +0800

    GridView control

--- FDLS/Views/GridView.swift
+++ FDLS/Views/GridView.swift
@@ -7,10 +7,38 @@
 
 import SwiftUI
 
+enum GridType: String, CaseIterable {
+  case fixed
+  case adaptive
+  case flexible
+}
+
+enum GridVAlign: String, CaseIterable {
+  case top
+  case center
+  case bottom
+}
+
+enum GridHAlign: String, CaseIterable {
+  case leading
+  case center
+  case trailing
+}
+
 struct GridView: View {
+  @State var gridType: GridType = .fixed
+  @State var gridVAlign: GridVAlign = .center
+  @State var gridHAlign: GridHAlign = .center
+  @State var itemWidth = 125.0
+  @State var columnCount = 1
+  @State var isFullItem = false
+  
   var body: some View {
     ZStack {
-      Color.red.ignoresSafeArea()
+      VStack {
+        Color.red.ignoresSafeArea()
+        Color.white.ignoresSafeArea()
+      }
 
       VStack {
         let d = UIDevice.current.name
@@ -19,36 +47,17 @@ struct GridView: View {
 
         ScrollView {
           LazyVGrid(
-//            columns: [
-//              GridItem(.fixed(70)),
-//              GridItem(.fixed(70)),
-//              GridItem(.fixed(70)),
-//          ],
-            
-//            columns: [
-//              GridItem(.adaptive(minimum: 200), spacing: 5, alignment: .leading), //水平間隔
-//          ],
-            
-            columns: Array.init(repeating: GridItem(.flexible()), count: 3),
-            
-//            columns: Array.init(repeating: GridItem(.flexible(), alignment: .topLeading), count: 7),
-            
-//            columns: [
-//              GridItem(.flexible(), alignment: .topLeading),
-//              GridItem(.flexible(), alignment: .top),
-//              GridItem(.flexible(), alignment: .topTrailing),
-//            ],
-            
-          alignment: .leading, //只能控水平對齊 (但還是以item為主)
-          spacing: 10, // 垂直間隔
-          pinnedViews: [.sectionHeaders, .sectionFooters]) {
+            columns: getGridItems(),
+            alignment: .leading, // 只能控水平對齊 (但還是以item為主)
+            spacing: 10, // 垂直間隔
+            pinnedViews: [.sectionHeaders, .sectionFooters])
+          {
             ForEach(0..<10) { section in
               Section {
                 let count = (section + 1) * 10
                 ForEach(0..<count) { item in
                   Text("\(section)-\(item)")
                     .padding()
-//                    .frame(maxWidth: .infinity)
                     .background(Color(UIColor.systemMint))
                 }
               } header: {
@@ -64,15 +73,86 @@ struct GridView: View {
           }
         }
         .background(.brown)
-        .padding(.vertical, 0.1)
-      }
-    } // fix over safe area
+        .padding(.vertical, 0.1) // fix over safe area
+        
+        Group {
+          Group {
+            Picker("", selection: $gridType) {
+              ForEach(GridType.allCases.indices, id: \.self) { idx in
+                Text("\(GridType.allCases[idx].rawValue)")
+                  .tag(GridType.allCases[idx]) // why only segmented style need tag???
+              }
+            }
+            Picker("", selection: $gridVAlign) {
+              ForEach(GridVAlign.allCases.indices, id: \.self) { idx in
+                Text("\(GridVAlign.allCases[idx].rawValue)")
+                  .tag(GridVAlign.allCases[idx])
+              }
+            }
+            Picker("", selection: $gridHAlign) {
+              ForEach(GridHAlign.allCases.indices, id: \.self) { idx in
+                Text("\(GridHAlign.allCases[idx].rawValue)")
+                  .tag(GridHAlign.allCases[idx])
+              }
+            }
+          }
+          .padding(.bottom, 8)
+          .pickerStyle(.segmented)
+          .colorMultiply(.gray)
+        
+          Slider(value: $itemWidth, in: 50 ... 200) {
+            Text("fucking confus uesless label")
+          } minimumValueLabel: {
+            Text("50")
+          } maximumValueLabel: {
+            Text("200")
+          }
+        
+          HStack {
+            Spacer()
+            Text("\(columnCount)")
+            Stepper("", value: $columnCount, in: 1 ... 10).labelsHidden()
+            Spacer()
+            Toggle("", isOn: $isFullItem).labelsHidden()
+            Spacer()
+          }
+        }.padding(.horizontal)
+      } // VStack
+    } // ZStack
+  }
+  
+  private func getGridItems() -> [GridItem] {
+    switch gridType {
+    case .fixed:
+      return Array(repeating: GridItem(.fixed(itemWidth), alignment: getAlignment()), count: columnCount)
+    case .adaptive:
+      return Array(repeating: GridItem(.adaptive(minimum: itemWidth), alignment: getAlignment()), count: columnCount)
+    case .flexible:
+      return Array(repeating: GridItem(.flexible(), alignment: getAlignment()), count: columnCount)
+    }
+  }
+  
+  func getAlignment() -> Alignment {
+    switch (gridVAlign, gridHAlign) {
+    case (.top, .leading): return .topLeading
+    case (.top, .center): return .top
+    case (.top, .trailing): return .topTrailing
+      
+    case (.center, .leading): return .leading
+    case (.center, .center): return .center
+    case (.center, .trailing): return .trailing
+      
+    case (.bottom, .leading): return .bottomLeading
+    case (.bottom, .center): return .bottom
+    case (.bottom, .trailing): return .bottomTrailing
+    }
   }
 }
 
 struct GridView_Previews: PreviewProvider {
   static var previews: some View {
-    GridView().previewDevice("iPhone 12 Pro")
-    GridView().previewDevice("iPhone 11 Pro Max")
+    GridView()
+//    GridView().previewDevice("iPhone 12 Pro")
+//    GridView().previewDevice("iPhone 11 Pro Max")
   }
 }

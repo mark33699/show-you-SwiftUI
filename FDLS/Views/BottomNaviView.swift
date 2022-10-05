0428a4de62b568ace8dfd5b94c96eccd947500f0
mark.HsieH@m1 <mark.hsieh@cerence.com>
2022-10-06 01:55:43 +0800

    TabView Navigation

--- FDLS/Views/BottomNaviView.swift
+++ FDLS/Views/BottomNaviView.swift
@@ -18,30 +18,50 @@ struct TabModel: Identifiable {
 }
 
 struct BottomNaviView: View {
-  
   let tabs = [
     TabModel(color: .red, name: "featured", imageName: "star"),
     TabModel(color: .orange, name: "bookmarks", imageName: "book"),
     TabModel(color: .yellow, name: "contacts", imageName: "person.crop.circle"),
     TabModel(color: .green, name: "recents", imageName: "clock"),
-    TabModel(color: .blue, name: "search", imageName: "magnifyingglass"),
+    TabModel(color: .cyan, name: "search", imageName: "magnifyingglass"),
 //    TabModel(color: .gray, name: "setting", imageName: "gearshape"),
   ]
   
+  @State var tabSelectedIndex = 0
+  
   var body: some View {
-    TabView {
-      ForEach(tabs) { tab in
-        tab.color
-          .overlay(content: {
-            Text(tab.title)
-          })
-          .tabItem {
-            Image(systemName: tab.imageName)
-            Text(tab.name)
-          }
-      }
+    TabView(selection: $tabSelectedIndex) {
+      
+      ForEach(tabs.indices) { index in
+        let tab = tabs[index]
+        NavigationView {
+          tab.color
+            .navigationTitle(Text(tab.name.uppercased()))
+            .overlay {
+              VStack {
+                Text(tab.title)
+                NavigationLink {
+                  NextView(color: tab.color.opacity(0.5), number: 1)
+                } label: {
+                  Text("Next!").tint(.white)
+                }.buttonStyle(.borderedProminent)
+              }
+            }
+        }
+        .tabItem {
+          Label(tab.name, systemImage: tab.imageName)
+        }
+        .tag(index)
+      } //ForEach
+      
+    } //TabView
+    .onChange(of: tabSelectedIndex) { newValue in
+      print("tab \(newValue) selected")
     }
-  }
+    
+    
+  } //Body
+  
   
   
 }
@@ -49,5 +69,22 @@ struct BottomNaviView: View {
 struct BottomNaviView_Previews: PreviewProvider {
   static var previews: some View {
     BottomNaviView()
+  }
+}
+
+struct NextView: View {
+  let color: Color
+  let number: Int
+  var body: some View {
+    color.overlay {
+      VStack {
+        Text("This is Next Number \(number)")
+        NavigationLink {
+          NextView(color: color.opacity(0.5), number: number + 1)
+        } label: {
+          Text("Next!").padding()
+        }
+      }
+    }
   }
 }

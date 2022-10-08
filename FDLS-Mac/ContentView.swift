224078d5e9e4c65ea7bb6cae05f869bc27e81ad3
MarkFly <mark.appledeveloper@gmail.com>
2022-10-08 21:50:32 +0800

    Add Mac Target

---
+++ FDLS-Mac/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FDLS-Mac
+//
+//  Created by FlyFat on 2022/10/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FDLS-Mac/FDLS_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  FDLS_MacApp.swift
+//  FDLS-Mac
+//
+//  Created by FlyFat on 2022/10/8.
+//
+
+import SwiftUI
+
+@main
+struct FDLS_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

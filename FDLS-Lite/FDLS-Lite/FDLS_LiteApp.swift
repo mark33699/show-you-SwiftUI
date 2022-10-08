3fc2a48d5b48668622d605b9d7ad0faad7ef01c2
MarkFly <mark.appledeveloper@gmail.com>
2022-10-09 02:26:44 +0800

    create lite project

---
+++ FDLS-Lite/FDLS-Lite/FDLS_LiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  FDLS_LiteApp.swift
+//  FDLS-Lite
+//
+//  Created by FlyFat on 2022/10/9.
+//
+
+import SwiftUI
+
+@main
+struct FDLS_LiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
